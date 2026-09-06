5bc6e100ab6bcda02ae53a3ba75cfbd4574fe5ec
Thiago <comsalmanteiga49@gmail.com>
2026-09-06 19:32:37 -0300

    feat: passwordHasher implement

--- src/modules/users/user.container.ts
+++ src/modules/users/user.container.ts
@@ -1,9 +1,11 @@
 import { UserController } from './user.controller.js'
 import { UserRepository } from './user.repository.js'
 import { UserService } from './user.service.js'
+import { BcryptPasswordHasher } from '../../@commons/utils/bcrypt.js'
 
 const userRepository = new UserRepository()
+const passwordHasher = new BcryptPasswordHasher()
 
-const userService = new UserService(userRepository)
+const userService = new UserService(userRepository, passwordHasher)
 
 export const userController = new UserController(userService)
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,10 +1,14 @@
 import type { UserRepository } from './user.repository.js'
+import type { PasswordHasher } from '../../@commons/utils/interfaces/passwordHasher.js'
 
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordHasher: PasswordHasher
+  ) {}
 
   async register(email: string, password: string) {
-    const passwordHash = password /// TODO: transformar em hash
+    const passwordHash = await this.passwordHasher.hashPassword(password)
 
     const user = await this.userRepository.register(email, passwordHash)
 
